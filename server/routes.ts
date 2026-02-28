70438473a2f9612e7e0b393866ae3c29e8029ea5
Adrien Ahza <adrienahza@gmail.com>
2026-02-28 19:29:11 +0700

    fix: PASTE KUNCI YANG BENAR

--- server/routes.ts
+++ server/routes.ts
@@ -508,7 +508,7 @@ app.get("/api/forex", async (req, res) => { const user = await getUser(req); res
           const ONE_SIGNAL_APP_ID = "b45b3256-b290-4a98-b5fa-afa0501a6b1c";
           
           // 1. PASTE KUNCI YANG SUKSES DI LAPTOP KE SINI
-          const REST_KEY = "os_v2_app_wrntevvssbfjrnp2v6qfagtldtb7mlaav2vezo4c73mpaee5rhtp2tyztuacwgddxk5qhd6etzx2rvfsf4lfpgosktb7mf75b2bo3ja";
+          const REST_KEY = "os_v2_app_wrntevvssbfjrnp2v6qfagtldss2ltv3wdbe54e456mrofdm57icygwhkfu2iufr5bnyzf7k55wach62z6cfyhvv7jy56esywggtfrq";
           const cleanKey = REST_KEY.replace(/[^a-zA-Z0-9_]/g, '');
 
           // 2. BUKTI CCTV: Kita paksa Vercel ngaku dia pakai kunci apa!
